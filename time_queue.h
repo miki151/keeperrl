54da3cae9c916bf9bc2a04ad16c6a2323592189e
Michal <michal@patagonia.(none)>
2014-04-26 11:25:34 +0200

    add license to source files

--- time_queue.h
+++ time_queue.h
@@ -1,3 +1,18 @@
+/* Copyright (C) 2013-2014 Michal Brzozowski (rusolis@poczta.fm)
+
+   This file is part of KeeperRL.
+
+   KeeperRL is free software; you can redistribute it and/or modify it under the terms of the
+   GNU General Public License as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   KeeperRL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along with this program.
+   If not, see http://www.gnu.org/licenses/ . */
+
 #ifndef _TIME_QUEUE_H
 #define _TIME_QUEUE_H
 
