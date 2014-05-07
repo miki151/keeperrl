fde3fd9d609f68b1820e7efd1472df67e53b562d
Michal <michal@patagonia.(none)>
2014-05-07 15:09:01 +0200

    add missing files

---
+++ vision.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "vision.h"
+#include "creature.h"
+
+template <class Archive> 
+void CreatureVision::serialize(Archive& ar, const unsigned int version) {
+}
+
+SERIALIZABLE(CreatureVision);
+
+template <class Archive> 
+void Vision::serialize(Archive& ar, const unsigned int version) {
+  ar & SVAR(inheritedFov) & SVAR(nightVision);
+  CHECK_SERIAL;
+}
+
+SERIALIZABLE(Vision);
+
+
+Vision::Vision(Vision* inherited, bool night) : inheritedFov(inherited), nightVision(night) {}
+
+Vision* Vision::getInheritedFov() const {
+  return inheritedFov;
+}
+
+bool Vision::isNightVision() const {
+  return nightVision;
+}
+
+void Vision::init() {
+  Vision::set(VisionId::ELF, new Vision(nullptr, false));
+  Vision::set(VisionId::NORMAL, new Vision(nullptr, false));
+  Vision::set(VisionId::NIGHT, new Vision(Vision::get(VisionId::NORMAL), true));
+}
+
---
+++ vision.h
@@ -0,0 +1,48 @@
+/* Copyright (C) 2013-2014 Michal Brzozowski (rusolis@poczta.fm)
+
+   This file is part of KeeperRL.
+
+   KeeperRL is free software; you can redistribute it and/or modify it under the terms of the
+   GNU General Public License as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   KeeperRL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along with this program.
+   If not, see http://www.gnu.org/licenses/ . */
+
+#ifndef _VISION_H
+#define _VISION_H
+
+#include "singleton.h"
+#include "enums.h"
+
+class Vision : public Singleton<Vision, VisionId> {
+  public:
+  Vision(Vision* inheritedFov, bool nightVision);
+  Vision* getInheritedFov() const;
+  bool isNightVision() const;
+  static void init();
+
+  SERIALIZATION_DECL(Vision);
+
+  private:
+  Vision* SERIAL(inheritedFov);
+  bool SERIAL(nightVision);
+};
+
+class Creature;
+class CreatureVision {
+  public:
+  virtual bool canSee(const Creature*, const Creature*) = 0;
+  virtual ~CreatureVision() {}
+
+  template <class Archive> 
+  void serialize(Archive& ar, const unsigned int version);
+};
+
+
+#endif
+
