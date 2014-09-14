f1a30f3cff29351607e1a63db513aae5568230c1
Michal <miki@patagonia>
2014-09-14 21:31:25 +0200

    add missing file

---
+++ minion_task.h
@@ -0,0 +1,34 @@
+#ifndef _MINION_TASK_H
+#define _MINION_TASK_H
+
+#include "util.h"
+
+RICH_ENUM(MinionTask,
+  SLEEP,
+  GRAVE,
+  TRAIN,
+  WORKSHOP,
+  STUDY,
+  LABORATORY,
+  PRISON,
+  TORTURE,
+  SACRIFICE,
+  EXECUTE,
+  WORSHIP,
+  LAIR,
+  EXPLORE,
+  COPULATE,
+  CONSUME,
+  RITUAL
+);
+
+RICH_ENUM(MinionTrait,
+  LEADER,
+  FIGHTER,
+  WORKER,
+  PRISONER,
+  NO_EQUIPMENT
+);
+
+
+#endif
