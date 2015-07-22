c9e2e6390e23d059dd6b96e356799a8340a66f51
Michal Brzozowski <miki@extremic.com>
2015-07-22 12:27:42 +0200

    Add wounded status to effects

--- creature.cpp
+++ creature.cpp
@@ -1083,6 +1083,10 @@ void Creature::setTime(double t) {
   time = t;
 }
 
+bool Creature::isBleeding() const {
+  return health < 0.5;
+}
+
 void Creature::tick(double realTime) {
   updateVision();
   if (Random.roll(5))
@@ -1110,7 +1114,7 @@ void Creature::tick(double realTime) {
     die(lastAttacker);
     return;
   }
-  if (health < 0.5) {
+  if (isBleeding()) {
     health -= delta / 40;
     playerMessage("You are bleeding.");
   }
@@ -2456,6 +2460,8 @@ vector<Creature::AdjectiveInfo> Creature::getBadAdjectives() const {
   vector<AdjectiveInfo> ret;
   if (!getWeapon())
     ret.push_back({"No weapon", ""});
+  if (health < 1)
+    ret.push_back({isBleeding() ? "Critically wounded" : "Wounded", ""});
   for (BodyPart part : ENUM_ALL(BodyPart))
     if (int num = attributes->injuredBodyParts[part])
       ret.push_back({getPlural("Injured " + attributes->getBodyPartName(part), num), ""});
