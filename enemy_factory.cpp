3c73fb04b5eb0f1c2e18b217987b5b011da841e3
Andrew Wright <aj268.wright@googlemail.com>
2017-08-05 04:29:34 +0100

    The landscape and tribes. The "tribe" of ghosts on the surface (which are actually friendly/technically wildlife) and below the surface demons and the demon boss. Place the demon den in mountain biomes but not actuall inside the mountains.

--- enemy_factory.cpp
+++ enemy_factory.cpp
@@ -120,6 +120,30 @@ EnemyInfo EnemyFactory::getById(EnemyId enemyId) {
               ImmigrantInfo(CreatureId::ORC, {MinionTrait::FIGHTER}).setFrequency(3),
               ImmigrantInfo(CreatureId::OGRE, {MinionTrait::FIGHTER}).setFrequency(1)
             }));
+    case EnemyId::DEMON_DEN_ABOVE:
+      return EnemyInfo(CONSTRUCT(SettlementInfo,
+            c.tribe = TribeId::getWildlife();
+            c.creatures = CreatureFactory::demonDenAbove(c.tribe);
+            c.buildingId = BuildingId::DUNGEON;
+            c.numCreatures = random.get(2, 3);
+            c.locationName = "Darkshrine Town"_s;
+            c.race = "ghosts"_s;
+            c.furniture = FurnitureFactory::dungeonOutside(c.tribe);
+            c.outsideFeatures = FurnitureFactory::dungeonOutside(c.tribe);),
+ 		    CollectiveConfig::noImmigrants());
+    case EnemyId::DEMON_DEN:
+      return EnemyInfo(CONSTRUCT(SettlementInfo,
+            c.tribe = TribeId::getMonster();
+            c.creatures = CreatureFactory::demonDen(c.tribe);
+            c.numCreatures = random.get(5, 10);
+            c.locationName = "Darkshrine"_s;
+            c.race = "demons"_s;
+            c.furniture = FurnitureFactory::dungeonOutside(c.tribe);
+            c.outsideFeatures = FurnitureFactory::dungeonOutside(c.tribe);),
+          CollectiveConfig::withImmigrants(300, 16, {
+              ImmigrantInfo(CreatureId::DEMON_DWELLER, {MinionTrait::FIGHTER}).setFrequency(1),
+          }), none,
+		  LevelConnection{LevelConnection::CRYPT, get(EnemyId::DEMON_DEN_ABOVE)});
     case EnemyId::VILLAGE:
       return EnemyInfo(CONSTRUCT(SettlementInfo,
             c.type = SettlementType::VILLAGE;
