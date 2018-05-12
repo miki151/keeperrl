1e0dcc32cb9044eb531fb0e4df35561622561adc
Andrew Wright <aj268.wright@googlemail.com>
2018-05-12 15:39:24 +0100

    Cooldown for shooting arrows with the bugs fixed.

--- creature.h
+++ creature.h
@@ -94,6 +94,7 @@ class Creature : public Renderable, public UniqueEntity<Creature>, public OwnedO
   bool canSee(Vec2) const;
   bool isEnemy(WConstCreature) const;
   void tick();
+  int getAmmoCooldownPoints() const;
 
   const CreatureName& getName() const;
   CreatureName& getName();
@@ -168,7 +169,7 @@ class Creature : public Renderable, public UniqueEntity<Creature>, public OwnedO
   CreatureAction stealFrom(Vec2 direction, const vector<WItem>&) const;
   CreatureAction give(WCreature whom, vector<WItem> items) const;
   CreatureAction payFor(const vector<WItem>&) const;
-  CreatureAction fire(Vec2 direction) const;
+  CreatureAction fire(Vec2 direction);
   CreatureAction construct(Vec2 direction, FurnitureType) const;
   CreatureAction whip(const Position&) const;
   bool canConstruct(FurnitureType) const;
@@ -298,6 +299,7 @@ class Creature : public Renderable, public UniqueEntity<Creature>, public OwnedO
 
   HeapAllocated<CreatureAttributes> SERIAL(attributes);
   Position SERIAL(position);
+  int SERIAL(ammoCooldownPoints);
   HeapAllocated<Equipment> SERIAL(equipment);
   HeapAllocated<optional<LevelShortestPath>> SERIAL(shortestPath);
   EntitySet<Creature> SERIAL(knownHiding);
