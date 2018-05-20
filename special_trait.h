18bb289696acd123c4199773eee72e55e2a74fc4
Andrew Wright <aj268.wright@googlemail.com>
2018-05-20 15:58:37 +0100

    Creatures with traits stand with words in their title.

--- special_trait.h
+++ special_trait.h
@@ -12,4 +12,7 @@ struct ExtraTraining {
 
 using SpecialTrait = variant<ExtraTraining, LastingEffect, SkillId>;
 
+string getSpecialTraitAdjective(LastingEffect effect);
+string getSpecialTraitAdjective(ExperienceType type);
+string getSpecialTraitJobName(SkillId skill);
 extern void applySpecialTrait(SpecialTrait, WCreature);
