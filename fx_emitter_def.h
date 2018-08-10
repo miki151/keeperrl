be5cc1ab29ed6f32080c6ff0056be9df006a5087
Krzysztof Jakubowski <nadult@fastmail.fm>
2018-08-10 14:50:09 +0200

    FX: refactor EmitterDef (improve strength & rotSpeed params)

--- fx_emitter_def.h
+++ fx_emitter_def.h
@@ -4,23 +4,43 @@
 
 namespace fx {
 
-// Emiterem mogą też być cząsteczki innego subsystemu ?
+// Defines behaviour of a particle system emitter.
+//
+//         frequency: how many particles are spawned per second
+//         direction: defines angle (in radians) or direction in which particles are emitted;
+//                    if angleSpread is big enough, base angle makes no difference
+//          strength: emission strength; affects particle movement and rotation speed
+//          rotSpeed: initial rotation speed; 50% of particles will rotate the other way
+// initialSpawnCount: how many particles are spawned at time 0
+//                    fractional value will speed up following emissions
+//
+//           *Spread: adds random(-spread, spread) to base value
+//                    should be >= 0
+//           *Curves: additional curves which may be used for complex FXes
+//
+// TODO: use vectors instead of angles ?
 struct EmitterDef {
   EmissionSource source;
-  Curve<float> frequency; // particles per second
-  Curve<float> strengthMin, strengthMax;
-  Curve<float> angle = 0.0f, angleSpread = fconstant::pi; // in radians
-  Curve<float> rotationSpeedMin, rotationSpeedMax;
+  Curve<float> frequency;
 
+  Curve<float> strength, strengthSpread;
+  Curve<float> direction, directionSpread = fconstant::pi;
+  Curve<float> rotSpeed, rotSpeedSpread;
   vector<Curve<float>> scalarCurves;
   vector<Curve<FVec3>> colorCurves;
-
   float initialSpawnCount = 0.0f;
 
-  // TODO: zamiast częstotliwości możemy mieć docelową ilość cząsteczek
-  // (danego rodzaju?) w danym momencie
-  // TODO: całkowanie niektórych krzywych?
-
-  string name;
+  void setStrengthSpread(Curve<float> base, Curve<float> spread) {
+    strength = std::move(base);
+    strengthSpread = std::move(spread);
+  }
+  void setRotSpeedSpread(Curve<float> base, Curve<float> spread) {
+    rotSpeed = std::move(base);
+    rotSpeedSpread = std::move(spread);
+  }
+  void setDirectionSpread(Curve<float> base, Curve<float> spread) {
+    direction = std::move(base);
+    directionSpread = std::move(spread);
+  }
 };
 }
--- fx_manager.cpp
+++ fx_manager.cpp
@@ -15,8 +15,6 @@ FXManager *FXManager::getInstance() { return s_instance; }
 
 FXManager::FXManager() {
   addDefaultDefs();
-  //saveDefs();
-
   CHECK(s_instance == nullptr && "There can be only one!");
   s_instance = this;
 }
@@ -66,7 +64,7 @@ void FXManager::simulateStable(double timeDelta, int desiredFps) {
   double desiredDelta = 1.0 / desiredFps;
   int numSteps = 0;
 
-  // TODO: czy chcemy jakoś przewidywać klatki?
+  // TODO: limit number of steps?
   while (timeDelta > desiredDelta) {
     simulate(desiredDelta);
     timeDelta -= desiredDelta;
