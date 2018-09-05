799e31e66ee2514abf540c7e786320b9a2716335
Krzysztof Jakubowski <nadult@fastmail.fm>
2018-09-05 17:07:42 +0200

    FX: Improve style in EmissionSource (remove m_)

--- fx_emission_source.cpp
+++ fx_emission_source.cpp
@@ -4,31 +4,31 @@
 
 namespace fx {
 
-EmissionSource::EmissionSource(FVec2 pos) : m_pos(pos), m_type(Type::point) {}
-EmissionSource::EmissionSource(FRect rect) : m_pos(rect.center()), m_param(rect.size() * 0.5f), m_type(Type::rect) {}
-EmissionSource::EmissionSource(FVec2 pos, float radius) : m_pos(pos), m_param(radius, 0.0f), m_type(Type::sphere) {}
+EmissionSource::EmissionSource(FVec2 pos) : pos(pos), type(Type::point) {}
+EmissionSource::EmissionSource(FRect rect) : pos(rect.center()), param(rect.size() * 0.5f), type(Type::rect) {}
+EmissionSource::EmissionSource(FVec2 pos, float radius) : pos(pos), param(radius, 0.0f), type(Type::sphere) {}
 
 FRect EmissionSource::rect() const {
-  PASSERT(m_type == Type::rect);
-  return {m_pos - m_param, m_pos + m_param};
+  PASSERT(type == Type::rect);
+  return {pos - param, pos + param};
 }
 float EmissionSource::sphereRadius() const {
-  PASSERT(m_type == Type::sphere);
-  return m_param.x;
+  PASSERT(type == Type::sphere);
+  return param.x;
 }
 
 FVec2 EmissionSource::sample(RandomGen &rand) const {
   // TODO: random get float as well
-  switch(m_type) {
+  switch (type) {
   case Type::point:
-    return m_pos;
+    return pos;
   case Type::rect:
-    return m_pos + FVec2(rand.getDouble(-m_param.x, m_param.x), rand.getDouble(-m_param.y, m_param.y));
+    return pos + FVec2(rand.getDouble(-param.x, param.x), rand.getDouble(-param.y, param.y));
   case Type::sphere: {
     FVec2 spoint(rand.getDouble(-1.0f, 1.0f), rand.getDouble(-1.0f, 1.0f));
     while(spoint.x * spoint.x + spoint.y * spoint.y > 1.0f)
       spoint = FVec2(rand.getDouble(-1.0f, 1.0f), rand.getDouble(-1.0f, 1.0f));
-    return m_pos + spoint * m_param.x;
+    return pos + spoint * param.x;
   }
   }
 
--- fx_emission_source.h
+++ fx_emission_source.h
@@ -10,24 +10,24 @@ RICH_ENUM(EmissionSourceType, point, rect, sphere);
 namespace fx {
 
 class EmissionSource {
-public:
+  public:
   using Type = EmissionSourceType;
 
-  EmissionSource() : m_type(Type::point) {}
+  EmissionSource() : type(Type::point) {}
   EmissionSource(FVec2 pos);
   EmissionSource(FRect rect);
   EmissionSource(FVec2 pos, float radius);
 
-  Type type() const { return m_type; }
-  FVec2 pos() const { return m_pos; }
+  Type getType() const { return type; }
+  FVec2 getPos() const { return pos; }
   FRect rect() const;
   float sphereRadius() const;
 
   // TODO(opt): sample multiple points at once
   FVec2 sample(RandomGen &) const;
 
-private:
-  FVec2 m_pos, m_param;
-  Type m_type;
+  private:
+  FVec2 pos, param;
+  Type type;
 };
 }
