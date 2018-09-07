1d621e9f939e393818cf8b332cbebfe2b7816b9f
Krzysztof Jakubowski <nadult@fastmail.fm>
2018-09-07 10:24:14 +0200

    FX: fix animation speed in different game & speed modes

--- map_gui.h
+++ map_gui.h
@@ -190,5 +190,7 @@ class MapGui : public GuiElem {
   EntityMap<Creature, milliseconds> woundedInfo;
   void considerWoundedAnimation(const ViewObject&, Color&, milliseconds curTimeReal);
 
+  bool lastFxTurnBased = false;
+  double lastFxTimeReal = -1.0, lastFxTimeTurn = -1.0;
   unique_ptr<FXViewManager> fxViewManager;
 };
