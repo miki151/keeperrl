7ac251109bdbd206c67582ab1f1b368777b39c61
Krzysztof Jakubowski <nadult@fastmail.fm>
2018-10-12 16:47:17 +0200

    FX: cleanup & optimization in Curve<> class

--- fx_curve.cpp
+++ fx_curve.cpp
@@ -2,63 +2,99 @@
 
 namespace fx {
 
-// TODO: there is a lot of potential for optimization
-
-template <class T> Curve<T>::Curve() = default;
-template <class T> Curve<T>::~Curve() = default;
-
-template <class T> Curve<T>::Curve(T value) : keys({0.0f}), values({value}), interp(InterpType::linear) {}
-
-template <class T>
-Curve<T>::Curve(vector<T> tvalues, InterpType interp) : values(std::move(tvalues)), interp(interp) {
-  keys.resize(values.size());
-  for (int n = 0; n < (int)keys.size(); n++)
-    keys[n] = float(n) / float(keys.size() - 1);
+template <class T> T interpCosine(const T &a, const T &b, float x) {
+  return lerp(a, b, (1.0f - std::cos(x * fconstant::pi)) * 0.5f);
 }
 
-template <class T>
-Curve<T>::Curve(vector<float> tkeys, vector<T> tvalues, InterpType interp)
-    : keys(std::move(tkeys)), values(std::move(tvalues)), interp(interp) {
+template <class T> T interpQuadratic(const T &v0, const T &v1, const T &v2, const T &v3, float x) {
+  // TODO: to chyba nie jest poprawne
+  return v1 * (1 - x) + v2 * x + (v2 - v3 + v1 - v0) * (1 - x) * x;
+}
+
+template <class T> T interpCubic(const T &y0, const T &y1, const T &y2, const T &y3, float mu) {
+  // Source: http://paulbourke.net/miscellaneous/
+  auto mu_sq = mu * mu;
+  // auto a0 = y3 - y2 - y0 + y1;
+  // auto a1 = y0 - y1 - a0;
+  // auto a2 = y2 - y0;
+  // auto a3 = y1;
+
+  auto a0 = -0.5f * y0 + 1.5f * y1 - 1.5f * y2 + 0.5f * y3;
+  auto a1 = y0 - 2.5f * y1 + 2.0f * y2 - 0.5f * y3;
+  auto a2 = -0.5f * y0 + 0.5f * y2;
+  auto a3 = y1;
+
+  return a0 * mu * mu_sq + a1 * mu_sq + a2 * mu + a3;
+}
+
+template <class T> Curve<T>::~Curve() = default;
+template <class T> Curve<T>::Curve(T value) : Curve({value}, InterpType::linear) {}
+
+template <class T> Curve<T>::Curve(vector<T> values, InterpType interp) : interp(interp) {
+  vector<float> keys(values.size());
+  for (int n = 0; n < (int)keys.size(); n++)
+    keys[n] = float(n) / float(keys.size() - 1);
+  initialize(keys, values);
+}
+
+template <class T> Curve<T>::Curve(vector<float> keys, vector<T> values, InterpType interp) : interp(interp) {
   DASSERT(keys.size() == values.size());
   for (int n = 0; n < (int)keys.size(); n++) {
     DASSERT(keys[n] >= 0.0f && keys[n] <= 1.0f);
     if (n > 0)
-      DASSERT(keys[n] >= keys[n - 1]);
+      DASSERT(keys[n] > keys[n - 1]);
   }
+  if (keys[0] != 0.0f) {
+    keys.insert(keys.begin(), 0.0f);
+    values.insert(values.begin(), values[0]);
+  }
+  if (keys[1] != 1.0f) {
+    keys.emplace_back(1.0f);
+    values.emplace_back(values.back());
+  }
+  initialize(keys, values);
 }
 
-// TODO: add sampleLooped
+template <class T> void Curve<T>::initialize(vector<float>& tkeys, vector<T>& tvalues) {
+  DASSERT(tkeys.size() <= maxKeys - 2);
+  DASSERT(tkeys.size() >= 1);
+
+  num_keys = tkeys.size();
+  for (int n = 0; n < num_keys; n++) {
+    keys[n] = tkeys[n];
+    values[n] = tvalues[n];
+  }
+  values[num_keys] = values[num_keys + 1] = values[num_keys - 1];
+  for (int n = 0; n < num_keys - 1; n++)
+    scale[n] = 1.0f / (keys[n + 1] - keys[n]);
+  scale[num_keys - 1] = 0.0f;
+}
+
 template <class T> T Curve<T>::sample(float position) const {
   PASSERT(position >= 0.0f && position <= 1.0f);
-  if (values.size() <= 1)
-    return values.empty() ? T() : values.front();
+  if (num_keys <= 1)
+    return values[0];
 
   int id = 0;
-  while (id < (int)keys.size() && keys[id] < position)
+  while (keys[id] < position)
     id++;
-  id = id == 0 ? 0 : id - 1;
-
-  int ids[4] = {id == 0 ? 0 : id - 1, id, id + 1, id + 2};
-
-  if (ids[2] == (int)keys.size())
-    ids[2] = ids[3] = id;
-  else if (ids[3] == (int)keys.size())
-    ids[3] = id + 1;
-
-  float key1 = keys[ids[1]], key2 = keys[ids[2]];
-  float t = position < key1 || key1 == key2 ? 0.0 : (position - key1) / (key2 - key1);
+  id = id - 1; // keys[0] is always 0.0f
+  float t = (position - keys[id]) * scale[id];
 
   switch (interp) {
   case InterpType::linear:
-    return lerp(values[ids[1]], values[ids[2]], t);
+    return lerp(values[id], values[id + 1], t);
   case InterpType::cosine:
-    return interpCosine(values[ids[1]], values[ids[2]], t);
-  case InterpType::quadratic:
-    return interpQuadratic(values[ids[0]], values[ids[1]], values[ids[2]], values[ids[3]], t);
-  case InterpType::cubic:
-    return interpCubic(values[ids[0]], values[ids[1]], values[ids[2]], values[ids[3]], t);
+    return interpCosine(values[id], values[id + 1], t);
+  case InterpType::quadratic: {
+    int prevId = id == 0 ? 0 : id - 1;
+    return interpQuadratic(values[prevId], values[id], values[id + 1], values[id + 2], t);
   }
-
+  case InterpType::cubic: {
+    int prevId = id == 0 ? 0 : id - 1;
+    return interpCubic(values[prevId], values[id], values[id + 1], values[id + 2], t);
+  }
+  }
   return T();
 }
 
