3b37f590c3c26d1b25c5d13c57dba8ccbabe64d4
Michal Brzozowski <miki@extremic.com>
2018-12-05 12:16:18 +0100

    Generate endless mode enemies hostile to the white keeper.

--- external_enemies.cpp
+++ external_enemies.cpp
@@ -94,7 +94,7 @@ void ExternalEnemies::update(WLevel level, LocalTime localTime) {
     Vec2 landingDir(Random.choose<Dir>());
     auto attackTask = getAttackTask(target, nextWave->enemy.behaviour);
     auto attackTaskRef = attackTask.get();
-    auto creatures = nextWave->enemy.creatures.generate(Random, TribeId::getHuman(),
+    auto creatures = nextWave->enemy.creatures.generate(Random, TribeId::getMonster(),
         MonsterAIFactory::singleTask(std::move(attackTask),
             nextWave->enemy.behaviour.getId() != AttackBehaviourId::HALLOWEEN_KIDS));
     for (auto& c : creatures) {
