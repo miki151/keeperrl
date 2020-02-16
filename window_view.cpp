f450e0219999f50fa9de50cf588c421d775d72fc
Michal Brzozowski <miki@extremic.com>
2020-02-17 10:11:28 +1100

    Fix crash when trying to teleport outside of map bounds.

--- window_view.cpp
+++ window_view.cpp
@@ -726,8 +726,8 @@ optional<Vec2> WindowView::chooseTarget(Vec2 playerPos, TargetType targetType, T
         return none;
       }
       if (pos && event.type == SDL::SDL_MOUSEBUTTONDOWN) {
-        if (event.button.button == SDL_BUTTON_LEFT &&
-            (targetType != TargetType::POSITION || passable[*pos] == PassableInfo::PASSABLE))
+        if (event.button.button == SDL_BUTTON_LEFT && (targetType != TargetType::POSITION ||
+            (pos->inRectangle(passable.getBounds()) && passable[*pos] == PassableInfo::PASSABLE)))
           return *pos;
         else
           return none;
