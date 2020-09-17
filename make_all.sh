07a81044ee3c6398ed361805ec531051889dac65
Michal Brzozowski <miki@extremic.com>
2020-09-17 10:46:16 +0200

    Fix make_all.sh

--- make_all.sh
+++ make_all.sh
@@ -3,9 +3,10 @@
 set -e
 
 sh ./windows_make.sh
-#sh ./win64_make.sh
+sh ./win64_make.sh
 sh ./osx_make.sh
 sh ./linux_make.sh
+rm unity.cpp
 
 cp -R data ~/sdk/tools/ContentBuilder/content/common/
 cp -R data_free ~/sdk/tools/ContentBuilder/content/common/
