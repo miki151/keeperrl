c9b200bdc6cf7402863ccb37cff3de6ef866eaa8
Michal Brzozowski <miki@extremic.com>
2020-09-19 14:35:25 +0200

    Fix make_all.sh

--- make_all.sh
+++ make_all.sh
@@ -6,7 +6,6 @@ sh ./windows_make.sh
 sh ./win64_make.sh
 sh ./osx_make.sh
 sh ./linux_make.sh
-rm unity.cpp
 
 cp -R data ~/sdk/tools/ContentBuilder/content/common/
 cp -R data_free ~/sdk/tools/ContentBuilder/content/common/
@@ -16,3 +15,5 @@ cd ~/sdk/tools/ContentBuilder/
 cd content
 ./print_versions.sh
 
+
+rm unity.cpp
