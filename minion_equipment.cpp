9b558b4a2b6acfdaff34f8d94c18469c882127e5
Michal Brzozowski <miki@extremic.com>
2021-01-18 14:51:08 +0100

    Consider all item upgrades minion equipment so they are dropped in equipment storage.

--- minion_equipment.cpp
+++ minion_equipment.cpp
@@ -63,7 +63,8 @@ optional<MinionEquipmentType> MinionEquipment::getEquipmentType(const Item* it) 
 bool MinionEquipment::isItemUseful(const Item* it) {
   static EnumSet<ItemClass> usefulItems {ItemClass::GOLD, ItemClass::POTION, ItemClass::SCROLL};
   return getEquipmentType(it) || usefulItems.contains(it->getClass()) || it->getAutomatonPart()
-      || (it->getClass() == ItemClass::FOOD && !it->getCorpseInfo()) || it->getIngredientType();
+      || (it->getClass() == ItemClass::FOOD && !it->getCorpseInfo()) || it->getIngredientType()
+      || it->getUpgradeInfo();
 }
 
 bool MinionEquipment::canUseItemType(const Creature* c, MinionEquipmentType type, const Item* it) const {
