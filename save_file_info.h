cbfab96df2140ed3c45e9439221325383c2c57c6
Michal Brzozowski <miki@extremic.com>
2021-03-12 13:21:59 +0100

    Fix platforms incompatibilit y in SaveFileInfo serialization.

--- save_file_info.h
+++ save_file_info.h
@@ -9,12 +9,7 @@ struct SaveFileInfo {
   optional<SteamId> SERIAL(steamId);
   template <class Archive>
   void serialize(Archive& ar, const unsigned int) {
-#ifdef WINDOWS
-    int SERIAL(dateTmp) = 0;
-#else
-    time_t dateTmp = 0;
-#endif
-    ar(filename, dateTmp, download, steamId);
+    ar(filename, download, steamId);
   }
   string getGameId() const {
     return filename.substr(0, filename.size() - 4);
