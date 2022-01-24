2c815755c9858d107b39e93459fbd2dd21945064
Michal Brzozowski <miki@extremic.com>
2022-01-24 10:37:23 +0100

    Update build scripts.

--- win64_make.sh
+++ win64_make.sh
@@ -4,7 +4,6 @@ set -e
 
 make clean
 time make -f Makefile-win -j10 AMD64=true UNITY=true RELEASE=true OPT=true DEBUG=true STEAMWORKS=true PREFIX=~/Downloads/mxe/usr/bin/x86_64-w64-mingw32.shared-
-mv keeper keeper.exe
 ls -l keeper.exe
 file keeper.exe
 mv keeper.exe ~/steam_content/windows/
--- windows_make.sh
+++ windows_make.sh
@@ -4,7 +4,6 @@ set -e
 
 make clean
 time make -f Makefile-win -j12 UNITY=true RELEASE=true OPT=true DEBUG=true STEAMWORKS=true PREFIX=~/Downloads/mxe/usr/bin/i686-w64-mingw32.shared.dw2-
-mv keeper keeper.exe
 ls -l keeper.exe
 file keeper.exe
 mv keeper.exe ~/steam_content/win32/
