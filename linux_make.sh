eae307d1d57b683831294dc1fe1a73989b492470
Michal Brzozowski <miki@extremic.com>
2022-05-23 16:06:28 +0200

    Fix release building settings

--- linux_make.sh
+++ linux_make.sh
@@ -3,6 +3,6 @@
 set -e
 
 make clean
-schroot --chroot steamrt_scout_amd64 -- make OPT=true RELEASE=true GCC=clang++-3.8 -j12
+schroot --chroot steamrt_scout_amd64 -- make OPT=true RELEASE=true GCC=clang++-3.8 -j32
 cp keeper ~/steam_content/linux64
 
