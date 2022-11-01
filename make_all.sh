81da495694b154be590a74fcc02d7c248b4904f7
Michal Brzozowski <miki@extremic.com>
2022-11-01 13:14:48 +0100

    Update release script to support building for a single platform

--- make_all.sh
+++ make_all.sh
@@ -2,10 +2,23 @@
 
 set -e
 
-sh ./windows_make.sh
-sh ./win64_make.sh
-sh ./osx_make.sh
-sh ./linux_make.sh
+if [ "$1" != "" ]; then
+	if [ "$1" = "linux" ]; then
+		sh ./linux_make.sh
+	fi
+	if [ "$1" = "osx" ]; then
+		sh ./osx_make.sh
+	fi
+	if [ "$1" = "windows" ]; then
+		sh ./windows_make.sh
+		sh ./win64_make.sh
+	fi
+else
+	sh ./windows_make.sh
+	sh ./win64_make.sh
+	sh ./osx_make.sh
+	sh ./linux_make.sh
+fi
 
 cp -R data ~/sdk/tools/ContentBuilder/content/common/
 cp -R data_free ~/sdk/tools/ContentBuilder/content/common/
