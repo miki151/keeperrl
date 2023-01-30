1e430cea2bb004428f729376a90c5306caedcf4f
Michal Brzozowski <miki@extremic.com>
2023-01-30 11:02:46 +0100

    Fix another compile error in unity mode

--- minion_page_index.cpp
+++ minion_page_index.cpp
@@ -3,11 +3,11 @@
 bool MinionPageIndex::left() {
   using namespace MinionPageElems;
   return visit(
-      [&](MinionAction a) {
+      [&](MinionPageElems::MinionAction a) {
         if (a.index == 0)
           assign(None{});
         else
-          assign(MinionAction{a.index - 1});
+          assign(MinionPageElems::MinionAction{a.index - 1});
         return true;
       },
       [&](Setting a) {
@@ -17,7 +17,7 @@ bool MinionPageIndex::left() {
           assign(Setting{a.index - 1});
         return true;
       },
-      [&](Equipment e) {
+      [&](MinionPageElems::Equipment e) {
         assign(None{});
         return true;
       },
@@ -29,15 +29,15 @@ bool MinionPageIndex::left() {
 bool MinionPageIndex::right(int numActions, int numSettings) {
   using namespace MinionPageElems;
   visit(
-    [&](MinionAction a) {
+    [&](MinionPageElems::MinionAction a) {
       if (a.index < numActions - 1)
-        assign(MinionAction{a.index + 1});
+        assign(MinionPageElems::MinionAction{a.index + 1});
     },
     [&](Setting a) {
       if (a.index < numSettings - 1)
         assign(Setting{a.index + 1});
     },
-    [&](None) { assign(MinionAction{0}); },
+    [&](None) { assign(MinionPageElems::MinionAction{0}); },
     [](const auto&) {}
   );
   return true;
@@ -47,12 +47,12 @@ bool MinionPageIndex::up(int numActions, int numSettings) {
   using namespace MinionPageElems;
   return visit(
     [&](Setting a) {
-      assign(MinionAction{min(numActions - 1, a.index)});
+      assign(MinionPageElems::MinionAction{min(numActions - 1, a.index)});
       return true;
     },
-    [&](Equipment e) {
+    [&](MinionPageElems::Equipment e) {
       if (e.index > 0)
-        assign(Equipment{e.index - 1});
+        assign(MinionPageElems::Equipment{e.index - 1});
       else
         assign(Setting{numSettings - 1});
       return true;
@@ -65,18 +65,18 @@ bool MinionPageIndex::up(int numActions, int numSettings) {
 bool MinionPageIndex::down(int numSettings, int numEquipment) {
   using namespace MinionPageElems;
   return visit(
-    [&](MinionAction a) {
+    [&](MinionPageElems::MinionAction a) {
       assign(Setting{min(numSettings - 1, a.index)});
       return true;
     },
     [&](Setting a) {
       if (numEquipment > 0)
-        assign(Equipment{0});
+        assign(MinionPageElems::Equipment{0});
       return true;
     },
-    [&](Equipment e) {
+    [&](MinionPageElems::Equipment e) {
       if (e.index < numEquipment - 1)
-        assign(Equipment{e.index + 1});
+        assign(MinionPageElems::Equipment{e.index + 1});
       return true;
     },
     [&](None) { return false; },
