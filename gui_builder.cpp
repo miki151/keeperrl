e3efccee48f95ef2c3aab7fdc44897d1e2b2611f
Robin Quint <rob2309@hotmail.de>
2024-03-16 21:55:09 +0100

    Revert "Fixed crash on world creation with disabled steamworks"
    This reverts commit 5d34e660d184d79ad663f58908df42788bb73ab0.

--- gui_builder.cpp
+++ gui_builder.cpp
@@ -815,11 +815,7 @@ int GuiBuilder::getImmigrationBarWidth() const {
 }
 
 bool GuiBuilder::hasController() const {
-  #ifdef USE_STEAMWORKS
   return !gui.getSteamInput()->controllers.empty();
-  #else
-  return false;
-  #endif
 }
 
 SGuiElem GuiBuilder::drawTutorialOverlay(const TutorialInfo& info) {
