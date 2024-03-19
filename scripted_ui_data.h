5713a3f7b7f743a82bd4e48eee7c5138b6ab13e6
Michal Brzozowski <miki@extremic.com>
2024-03-19 09:32:49 +0100

    Optimize message list UI by using a cache to store paragraph heights

--- scripted_ui_data.h
+++ scripted_ui_data.h
@@ -92,6 +92,7 @@ struct ScriptedUIState {
   optional<int> highlightedElem;
   unordered_map<int, ScriptedUIDataElems::SliderState> sliderState;
   unordered_map<int, milliseconds> tooltipTimeouts;
+  unordered_map<string, int> paragraphSizeCache;
   ScriptedUIData highlightNext = ScriptedUIDataElems::Callback{
       [&elem = this->highlightedElem] { elem = elem.value_or(-1) + 1; return false; }};
   ScriptedUIData highlightPrevious = ScriptedUIDataElems::Callback{
