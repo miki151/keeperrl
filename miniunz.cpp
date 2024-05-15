a79ce7bfbeda97a42da2fac0365c1d44ab953e80
thfrwn <11335318+rfht@users.noreply.github.com>
2024-05-15 07:07:52 -0400

    fix unsafe string functions

--- miniunz.cpp
+++ miniunz.cpp
@@ -117,7 +117,7 @@ int makedir (
     return 0;
 
   buffer = (char*)malloc(len+1);
-  strcpy(buffer,newdir);
+  strlcpy(buffer,newdir,len+1);
 
   if (buffer[len-1] == '/') {
     buffer[len-1] = '\0';
