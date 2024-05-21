ee4107e979828ff2bb1bd722fbc4beaa32ed743c
Michal Brzozowski <miki@extremic.com>
2024-05-21 11:31:29 +0200

    Add ClaimTile effect

--- keeper_base_info.cpp
+++ keeper_base_info.cpp
@@ -1,8 +1,8 @@
 #include "keeper_base_info.h"
 #include "pretty_archive.h"
 
-void KeeperBaseInfo::serialize(PrettyInputArchive& ar, unsigned int version) {
-  if (ar.eatMaybe("insideMountain"))
+void KeeperBaseInfo::serialize(PrettyInputArchive& ar1, unsigned int version) {
+  if (ar1.eatMaybe("insideMountain"))
     insideMountain = true;
-  ar(size, layout);
+  ar1(size, layout);
 }
