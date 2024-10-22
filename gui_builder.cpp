d92ecff9f1677bac4a7cc1ccf38fc90011e7b420
Michal Brzozowski <miki@extremic.com>
2024-10-22 11:22:43 +0200

    Fix a UI icon

--- gui_builder.cpp
+++ gui_builder.cpp
@@ -3904,7 +3904,7 @@ static ViewId getViewId(MinionActivity option) {
     case MinionActivity::BE_EXECUTED: return ViewId("gallows");
     case MinionActivity::PHYLACTERY: return ViewId("phylactery");
     case MinionActivity::PREACHING: return ViewId("rostrum_wood");
-    case MinionActivity::MASS: return ViewId("pew");
+    case MinionActivity::MASS: return ViewId("pew_wood");
     case MinionActivity::PRAYER: return ViewId("prayer_bench_wood");
     case MinionActivity::HEARING_CONFESSION:
     case MinionActivity::CONFESSION: return ViewId("confessional");
