0a76d2e2055b492efb242237c109f177b66b27e4
Michal Brzozowski <miki@extremic.com>
2025-01-30 15:51:51 +0100

    Fix spell schools data file

--- spell_school.cpp
+++ spell_school.cpp
@@ -5,8 +5,12 @@ SERIALIZE_DEF(SpellSchool, expType, spells, name)
 SERIALIZATION_CONSTRUCTOR_IMPL(SpellSchool)
 
 void SpellSchool::serialize(PrettyInputArchive& ar1, const unsigned int) { \
-  if (ar1.peek()[0] == '\"') {
-    ar1(name, expType, spells);
-  } else
+  auto bookmark = ar1.bookmark();
+  auto s1 = ar1.eat();
+  auto s2 = ar1.eat();
+  ar1.seek(bookmark);
+  if (s2[0] == '{')
     ar1(expType, spells);
+  else
+    ar1(name, expType, spells);
 }
